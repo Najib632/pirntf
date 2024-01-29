fdd3acf9a7e0d41f0458874c713367427c4613f9
Najib Muhammad <najibm38@mail.com>
2024-01-29 09:13:49 +0100

    Handle c,s and %

--- main.h
+++ main.h
@@ -1,7 +1,28 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdarg.h>
+#include <stdlib.h>
+
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+
+/**
+ * struct format_s - format parser
+ * @spec: format specifier
+ * @handle: pointer to a function
+ **/
+typedef struct format_s
+{
+	char *spec;
+	int (*handle)(va_list);
+} format_t;
+
+int (*get_fmt(char spec))(va_list);
+int handle_c(va_list args);
+int handle_s(va_list args);
+int handle_p(va_list args);
+int handle_d(va_list args);
 
 #endif /*_MAIN_H_*/
